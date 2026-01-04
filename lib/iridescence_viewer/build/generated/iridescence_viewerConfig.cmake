034100db45592ca99b4e8e7c62360dd8d82562d5
Ardy Seto Priambodo <2black0@gmail.com>
2026-01-04 08:53:29 +0700

    update gitignonre

--- lib/iridescence_viewer/build/generated/iridescence_viewerConfig.cmake
+++
@@ -1,6 +0,0 @@
-include(CMakeFindDependencyMacro)
-
-list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR})
-find_dependency(Iridescence)
-
-include("${CMAKE_CURRENT_LIST_DIR}/iridescence_viewerTargets.cmake")
